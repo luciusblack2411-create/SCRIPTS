b1c19bdd406edf7d581b96049775aee43a6a69aa
luciusblack2411-create <luciusblack2411@gmail.com>
2026-08-22 13:02:02 -0600

    feat(runner): export assessment plan API

--- src/cisco_assessment/runner/__init__.py
+++ src/cisco_assessment/runner/__init__.py
@@ -2,15 +2,20 @@
 
 from .errors import AssessmentRunnerError, RunnerFailure, RunnerStage
 from .factory import build_default_runner, build_runner
-from .models import AssessmentRunnerResult
+from .models import AssessmentCommandResult, AssessmentRunnerResult
+from .plan import AssessmentPlan, AssessmentPlanItem, SHOW_VERSION_PLAN_V0_2
 from .service import AssessmentRunner
 
 __all__ = [
+    "AssessmentCommandResult",
+    "AssessmentPlan",
+    "AssessmentPlanItem",
     "AssessmentRunner",
     "AssessmentRunnerError",
     "AssessmentRunnerResult",
     "RunnerFailure",
     "RunnerStage",
+    "SHOW_VERSION_PLAN_V0_2",
     "build_default_runner",
     "build_runner",
 ]
