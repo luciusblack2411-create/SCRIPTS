e04d419d2c26a080fd9978d485efe28612afbebb
luciusblack2411-create <luciusblack2411@gmail.com>
2026-08-22 18:20:49 -0600

    feat(parsers): export interface status parser

--- src/cisco_assessment/parsers/ios/__init__.py
+++ src/cisco_assessment/parsers/ios/__init__.py
@@ -1,6 +1,11 @@
 """Cisco IOS/IOS-XE parser implementations."""
 
+from .show_interfaces_status import IOSShowInterfacesStatusParser
 from .show_inventory import IOSShowInventoryParser
 from .show_version import IOSShowVersionParser
 
-__all__ = ["IOSShowInventoryParser", "IOSShowVersionParser"]
+__all__ = [
+    "IOSShowInterfacesStatusParser",
+    "IOSShowInventoryParser",
+    "IOSShowVersionParser",
+]
