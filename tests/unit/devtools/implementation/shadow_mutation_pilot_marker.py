b33539ff1df5eddf658a868dddd129ba62cb7fca
luciusblack2411-create <luciusblack2411@gmail.com>
2026-08-23 13:17:42 -0600

    test(devtools): shadow work-branch mutation pilot

---
+++ tests/unit/devtools/implementation/shadow_mutation_pilot_marker.py
@@ -0,0 +1,7 @@
+"""Shadow mutation pilot marker for IMPLEMENTATION_AGENT_V1.
+
+This file exists only on the dedicated shadow-pilot work branch and must never be merged.
+"""
+
+PILOT_BASE_SHA = "39801210d56f0dfc2fb7cdc1a87609636e0d0bec"
+PILOT_MODE = "WORK_BRANCH"
