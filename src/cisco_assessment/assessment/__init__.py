b4d3ce00e2ac6c9af04cef009978ed95041f9c9f
luciusblack2411-create <luciusblack2411@gmail.com>
2026-08-22 18:59:52 -0600

    feat(rules): export interface observation catalog

--- src/cisco_assessment/assessment/__init__.py
+++ src/cisco_assessment/assessment/__init__.py
@@ -19,19 +19,31 @@ from .hardware_inventory_rules import (
     UniqueInventorySerialsRule,
     hardware_inventory_rule_catalog,
 )
+from .interface_observation_rules import (
+    INTERFACE_OBSERVATION_RULES,
+    ConnectedInterfacesObservedRule,
+    DisabledInterfacesObservedRule,
+    ErrDisabledInterfacesRule,
+    UnrecognizedInterfaceStatusRule,
+    interface_observation_rule_catalog,
+)
 from .models import AssessmentResult, Finding, RuleDecision, RuleMetadata, RuleOutcome
 from .rules import AssessmentRule
 
 __all__ = [
     "DEVICE_INFO_RULES",
     "HARDWARE_INVENTORY_RULES",
+    "INTERFACE_OBSERVATION_RULES",
     "AssessmentContext",
     "AssessmentEngine",
     "AssessmentResult",
     "AssessmentRule",
     "AssessmentStatus",
     "ChassisIdentityObservedRule",
+    "ConnectedInterfacesObservedRule",
+    "DisabledInterfacesObservedRule",
     "DuplicateRuleError",
+    "ErrDisabledInterfacesRule",
     "EvidenceRequest",
     "Finding",
     "FindingEvidence",
@@ -48,6 +60,8 @@ __all__ = [
     "SoftwareVersionObservedRule",
     "SourceTrace",
     "UniqueInventorySerialsRule",
+    "UnrecognizedInterfaceStatusRule",
     "device_info_rule_catalog",
     "hardware_inventory_rule_catalog",
+    "interface_observation_rule_catalog",
 ]
