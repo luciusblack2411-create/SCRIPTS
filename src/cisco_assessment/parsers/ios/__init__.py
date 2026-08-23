69a931aa2d560ea00b7ca189c4ede4a27dcfdfc9
luciusblack2411-create <luciusblack2411@gmail.com>
2026-08-22 22:29:19 -0600

    feat(parser): export show vlan brief parser

--- src/cisco_assessment/parsers/ios/__init__.py
+++ src/cisco_assessment/parsers/ios/__init__.py
@@ -3,9 +3,11 @@
 from .show_interfaces_status import IOSShowInterfacesStatusParser
 from .show_inventory import IOSShowInventoryParser
 from .show_version import IOSShowVersionParser
+from .show_vlan_brief import IOSShowVlanBriefParser
 
 __all__ = [
     "IOSShowInterfacesStatusParser",
     "IOSShowInventoryParser",
     "IOSShowVersionParser",
+    "IOSShowVlanBriefParser",
 ]
